fa73827c154cb68d574fd8482bc535c0de175552
Neil <neilrowland56@yahoo.com>
2022-11-20 10:14:02 +0000

    first commit- next tailwind prisma installed Layout and form templates

---
+++ app/login/page.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    
+    <div className="  flex justify-center items-center flex-col gap-y-4 center mt-5 min-h-[600px] ">
+        <h2 className='text-yellow-600 text-5xl font-extrabold'>Welcome to Kudos</h2>
+        <p className="font-semibold text-slate-300">Log In To Give Some Praise!</p>
+
+        <form method="post" className="rounded-2xl bg-gray-200 p-6 w-96">
+          <label htmlFor="email" className="text-blue-600 font-semibold">
+            Email
+          </label>
+          <input type="text" id="email" name="email" className="w-full p-2 rounded-xl my-2" />
+
+          <label htmlFor="password" className="text-blue-600 font-semibold">
+            Password
+          </label>
+          <input type="password" id="password" name="password" className="w-full p-2 rounded-xl my-2" />
+
+          <div className="w-full text-center">
+            <input
+              type="submit"
+              className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+              value="Sign In"
+            />
+          </div>
+        </form>
+      
+    </div>
+    
+    
+  )
+}
+
+export default LoginPage
